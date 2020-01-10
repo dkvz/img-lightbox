2179fd677ff966480f069a95b101111a280ea71c
dkvz <bouc@clanz.be>
2020-01-10 13:34:56 +0100

    Remove the inlining of SVG for the no-shadow build

--- src/no-shadow/img-lightbox.js
+++ src/no-shadow/img-lightbox.js
@@ -35,7 +35,13 @@ class ImgLightbox extends HTMLElement {
     this.loadingOverlay.style.backgroundColor = 'rgba(0,0,0,.2)';
     this.loadingOverlay.style.alignItems = 'center';
     this.loadingOverlay.style.justifyContent = 'center';
-    this.loadingOverlay.innerHTML = this.loaderSvg;
+    const loaderImage = document.createElement('img');
+    loaderImage.src = this.loaderSvgUrl;
+    loaderImage.style.width = '50%';
+    loaderImage.style.opacity = '0.6';
+    loaderImage.style.animation = 'lightbox-loader 2s infinite';
+    this.loadingOverlay.appendChild(loaderImage);
+    //this.loadingOverlay.innerHTML = this.loaderSvg;
     this.hideLoaderOverlay();
     this.appendChild(this.loadingOverlay);
   }
@@ -49,7 +55,7 @@ class ImgLightbox extends HTMLElement {
       // Show the spinner overlay:
       this.showLoaderOverlay();
       if (!this.img) {
-        this.img = new Image();
+        this.img = document.createElement('img');
         this.img.src = this.fullImage;
         this.img.addEventListener('load', () => this.showImage(this.img));
       } else {
@@ -121,27 +127,9 @@ class ImgLightbox extends HTMLElement {
 // I could not inline the SVG with Parcel, so I copied it here.
 // That should be optimized at a later date as I'm also copying it to 
 // the shadow DOM version.
-ImgLightbox.prototype.loaderSvg = `
-<svg enable-background="new 0 0 64 64" version="1.1" viewBox="0 0 64 64" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><metadata><rdf:RDF><cc:Work rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage"/><dc:title/></cc:Work></rdf:RDF></metadata>
-<g>
-		<circle class="st0" cx="32" cy="32" r="32"/>
-	</g><g class="st1" opacity=".2">
-		<path class="st2" d="m46 50h-2v-4c0-4.5-3-7.4-3-7.4s-3-2.8-3.9-3.7 0-1.8 0-1.8 1.3-1.3 4.2-4c2.8-2.7 2.7-6.7 2.7-6.7v-5.1l-12-1-12 1v5.1s-0.1 3.9 2.7 6.7c2.8 2.7 4.2 4 4.2 4s0.9 0.9 0 1.8-3.9 3.7-3.9 3.7-3 2.8-3 7.4v4h-2c-1.1 0-2 0.9-2 2s0.9 2 2 2h28c1.1 0 2-0.9 2-2s-0.9-2-2-2z" fill="#231f20"/>
-	</g>
-		<path class="st3" d="m41 36.6s-3-2.8-3.9-3.7 0-1.8 0-1.8 1.3-1.3 4.2-4c2.8-2.7 2.7-6.7 2.7-6.7v-5.1l-12-1-12 1v5.1s-0.1 3.9 2.7 6.7c2.8 2.7 4.2 4 4.2 4s0.9 0.9 0 1.8-3.9 3.7-3.9 3.7-3 2.8-3 7.4v5.1h24v-5.1c0-4.5-3-7.4-3-7.4z" fill="#fff"/>
-	
-		<path class="st4" d="m31 43v-11c0-2.1-1-3.4-1.3-3.7l-4.2-4c-1.5-1.4-1.5-3.7-1.5-3.7h16s0 2.3-1.5 3.7l-4.2 4c-0.4 0.3-1.3 1.6-1.3 3.7v11z"/>
-	<g class="st5" opacity=".3">
-		<path class="st2" d="m48 16c0 1.1-0.9 2-2 2h-28c-1.1 0-2-0.9-2-2s0.9-2 2-2h28c1.1 0 2 0.9 2 2z" fill="#231f20"/>
-	</g><g fill="#fff">
-		<path class="st6" d="m48 14c0 1.1-0.9 2-2 2h-28c-1.1 0-2-0.9-2-2s0.9-2 2-2h28c1.1 0 2 0.9 2 2z" fill="#fff"/>
-	</g><g fill="#fff">
-		<path class="st6" d="m48 50c0 1.1-0.9 2-2 2h-28c-1.1 0-2-0.9-2-2s0.9-2 2-2h28c1.1 0 2 0.9 2 2z" fill="#fff"/>
-	</g><g>
-		<polygon class="st4" points="23 48 23 48 32 40 41 48"/>
-	</g>
-
-</svg>
-`;
+// Eagerly creating all these elements on page with lots of img-lightbox elements
+// would be a really bad idea.
+// This should not be inlined anyway. MAKE SURE IT'S NOT BEING INLINED.
+ImgLightbox.prototype.loaderSvgUrl = require('../../assets/hourglass.svg');
 
 export default ImgLightbox;
--- src/shadow/img-lightbox.js
+++ src/shadow/img-lightbox.js
@@ -39,7 +39,7 @@ class ImgLightbox extends HTMLElement {
     this.loadingOverlay.style.justifyContent = 'center';
     this.loadingOverlay.innerHTML = this.loaderSvg;
     this.hideLoaderOverlay();
-    this.appendChild(this.loadingOverlay);
+    this.shadowRoot.appendChild(this.loadingOverlay);
   }
 
   showLightbox(e) {
@@ -95,7 +95,7 @@ class ImgLightbox extends HTMLElement {
         (type) => this.overlay.addEventListener(type, closeOverlay)
       );
 
-      this.appendChild(this.overlay);
+      this.shadowRoot.appendChild(this.overlay);
     }
     img.style.transform = 'scale(0.1)';
     // Hide the loading overlay:
@@ -120,6 +120,8 @@ class ImgLightbox extends HTMLElement {
 
 }
 
+// We should still try to inline the loader with the
+// module bundler rahter than doing this.
 ImgLightbox.prototype.template = /*template*/`
 <style>
   :host {
@@ -127,32 +129,30 @@ ImgLightbox.prototype.template = /*template*/`
   }
 </style>
 <slot />
-`;
-
-// I could not inline the SVG with Parcel, so I copied it here.
-// That should be optimized at a later date as I'm also copying it to 
-// the shadow DOM version.
-ImgLightbox.prototype.loaderSvg = `
-<svg enable-background="new 0 0 64 64" version="1.1" viewBox="0 0 64 64" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><metadata><rdf:RDF><cc:Work rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage"/><dc:title/></cc:Work></rdf:RDF></metadata>
-<g>
-		<circle class="st0" cx="32" cy="32" r="32"/>
-	</g><g class="st1" opacity=".2">
-		<path class="st2" d="m46 50h-2v-4c0-4.5-3-7.4-3-7.4s-3-2.8-3.9-3.7 0-1.8 0-1.8 1.3-1.3 4.2-4c2.8-2.7 2.7-6.7 2.7-6.7v-5.1l-12-1-12 1v5.1s-0.1 3.9 2.7 6.7c2.8 2.7 4.2 4 4.2 4s0.9 0.9 0 1.8-3.9 3.7-3.9 3.7-3 2.8-3 7.4v4h-2c-1.1 0-2 0.9-2 2s0.9 2 2 2h28c1.1 0 2-0.9 2-2s-0.9-2-2-2z" fill="#231f20"/>
-	</g>
-		<path class="st3" d="m41 36.6s-3-2.8-3.9-3.7 0-1.8 0-1.8 1.3-1.3 4.2-4c2.8-2.7 2.7-6.7 2.7-6.7v-5.1l-12-1-12 1v5.1s-0.1 3.9 2.7 6.7c2.8 2.7 4.2 4 4.2 4s0.9 0.9 0 1.8-3.9 3.7-3.9 3.7-3 2.8-3 7.4v5.1h24v-5.1c0-4.5-3-7.4-3-7.4z" fill="#fff"/>
-	
-		<path class="st4" d="m31 43v-11c0-2.1-1-3.4-1.3-3.7l-4.2-4c-1.5-1.4-1.5-3.7-1.5-3.7h16s0 2.3-1.5 3.7l-4.2 4c-0.4 0.3-1.3 1.6-1.3 3.7v11z"/>
-	<g class="st5" opacity=".3">
-		<path class="st2" d="m48 16c0 1.1-0.9 2-2 2h-28c-1.1 0-2-0.9-2-2s0.9-2 2-2h28c1.1 0 2 0.9 2 2z" fill="#231f20"/>
-	</g><g fill="#fff">
-		<path class="st6" d="m48 14c0 1.1-0.9 2-2 2h-28c-1.1 0-2-0.9-2-2s0.9-2 2-2h28c1.1 0 2 0.9 2 2z" fill="#fff"/>
-	</g><g fill="#fff">
-		<path class="st6" d="m48 50c0 1.1-0.9 2-2 2h-28c-1.1 0-2-0.9-2-2s0.9-2 2-2h28c1.1 0 2 0.9 2 2z" fill="#fff"/>
-	</g><g>
-		<polygon class="st4" points="23 48 23 48 32 40 41 48"/>
-	</g>
-
-</svg>
+<div id="overlay">
+  
+</div>
+<div id="loader">
+  <svg enable-background="new 0 0 64 64" version="1.1" viewBox="0 0 64 64" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><metadata><rdf:RDF><cc:Work rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage"/><dc:title/></cc:Work></rdf:RDF></metadata>
+  <g>
+      <circle class="st0" cx="32" cy="32" r="32"/>
+    </g><g class="st1" opacity=".2">
+      <path class="st2" d="m46 50h-2v-4c0-4.5-3-7.4-3-7.4s-3-2.8-3.9-3.7 0-1.8 0-1.8 1.3-1.3 4.2-4c2.8-2.7 2.7-6.7 2.7-6.7v-5.1l-12-1-12 1v5.1s-0.1 3.9 2.7 6.7c2.8 2.7 4.2 4 4.2 4s0.9 0.9 0 1.8-3.9 3.7-3.9 3.7-3 2.8-3 7.4v4h-2c-1.1 0-2 0.9-2 2s0.9 2 2 2h28c1.1 0 2-0.9 2-2s-0.9-2-2-2z" fill="#231f20"/>
+    </g>
+      <path class="st3" d="m41 36.6s-3-2.8-3.9-3.7 0-1.8 0-1.8 1.3-1.3 4.2-4c2.8-2.7 2.7-6.7 2.7-6.7v-5.1l-12-1-12 1v5.1s-0.1 3.9 2.7 6.7c2.8 2.7 4.2 4 4.2 4s0.9 0.9 0 1.8-3.9 3.7-3.9 3.7-3 2.8-3 7.4v5.1h24v-5.1c0-4.5-3-7.4-3-7.4z" fill="#fff"/>
+    
+      <path class="st4" d="m31 43v-11c0-2.1-1-3.4-1.3-3.7l-4.2-4c-1.5-1.4-1.5-3.7-1.5-3.7h16s0 2.3-1.5 3.7l-4.2 4c-0.4 0.3-1.3 1.6-1.3 3.7v11z"/>
+    <g class="st5" opacity=".3">
+      <path class="st2" d="m48 16c0 1.1-0.9 2-2 2h-28c-1.1 0-2-0.9-2-2s0.9-2 2-2h28c1.1 0 2 0.9 2 2z" fill="#231f20"/>
+    </g><g fill="#fff">
+      <path class="st6" d="m48 14c0 1.1-0.9 2-2 2h-28c-1.1 0-2-0.9-2-2s0.9-2 2-2h28c1.1 0 2 0.9 2 2z" fill="#fff"/>
+    </g><g fill="#fff">
+      <path class="st6" d="m48 50c0 1.1-0.9 2-2 2h-28c-1.1 0-2-0.9-2-2s0.9-2 2-2h28c1.1 0 2 0.9 2 2z" fill="#fff"/>
+    </g><g>
+      <polygon class="st4" points="23 48 23 48 32 40 41 48"/>
+    </g>
+  </svg>
+</div>
 `;
 
 export default ImgLightbox;
